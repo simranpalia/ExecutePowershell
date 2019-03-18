0cfd1e157237de90bacf4669afe7d757682e8194
Simran <simran_palia@yahoo.co.in>
2019-03-18 16:14:15 +0800

    Added System.Linq reference to search.

--- ExecutePowershell/Program.cs
+++ ExecutePowershell/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Management.Automation;
+using System.Threading;
+using System.Linq;
 
 namespace ExecutePowershell
 {
@@ -6,7 +11,19 @@ namespace ExecutePowershell
     {
         static void Main(string[] args)
         {
+            List<string> processesToKill = new List<string>();
 
+            processesToKill.Add("Chrome");
+
+            var processes = Process.GetProcesses();
+
+            foreach (var item in processes)
+            {
+                if (processesToKill.Any(x => x == item.ProcessName))
+                {
+                    Console.WriteLine("Killing... " + item.ProcessName);
+                }
+            }
         }
     }
 }
